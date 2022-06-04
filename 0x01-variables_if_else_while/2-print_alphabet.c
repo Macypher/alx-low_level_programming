d308962778db26f90a5c80079a91eedf8d431a51
Macypher <ugoski135@gmail.com>
2022-06-03 17:53:22 -0700

    checker reloaded

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 /**
  * main - prints the alphabet in lowercase,
@@ -6,12 +7,12 @@
  */
 int main(void)
 {
-        char ch;
+ char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
+ for (ch = 'a'; ch <= 'z'; ch++)
+ {
+  putchar(ch);
+ }
+ putchar('\n');
+ return (0);
 }
